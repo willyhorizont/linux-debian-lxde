d4560f33b2e8eb85b451f627369440e8b90e5f94
willyhorizont <willyhorizont@gmail.com>
2026-09-17 06:17:46 +0700

    [Last updated: 17 Sep 2026 @ 06:17 AM][version: 0.1.5] update bluetooth indicator, add timeout;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.1.4" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.1.5" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -12,8 +12,7 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update plank theme;
-TODO: configure plank theme;
+update bluetooth indicator, add timeout;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
