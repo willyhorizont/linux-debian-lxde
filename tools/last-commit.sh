00209638cd7a7fdc125fd5fde94ca9bdd7a3e27e
willyhorizont <willyhorizont@gmail.com>
2026-09-17 09:35:32 +0700

    [Last updated: 17 Sep 2026 @ 09:35 AM][version: 0.1.9] update tint2-bottom, fix suckless-bluetooth attempt 4; update screenshot;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.1.8" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.1.9" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -12,9 +12,8 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update tint2-top;
-update tint2-bottom;
-update plank theme;
+update tint2-bottom, fix suckless-bluetooth attempt 4;
+update screenshot;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
