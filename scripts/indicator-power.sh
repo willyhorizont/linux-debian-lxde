b4c0181414c122ff23c1164773de5c294e00bccc
willyhorizont <willyhorizont@gmail.com>
2026-09-19 07:38:51 +0700

    [Last updated: 19 Sep 2026 @ 07:38 AM][version: 0.2.7] update indicator-power.sh;

--- scripts/indicator-power.sh
+++ scripts/indicator-power.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d /sys/class/power_supply ] || [ -z "$(ls /sys/class/power_supply/ | grep -E '^BAT|^battery')" ]; then
-    echo "AC=y ; batt=n     "
+    echo "AC=y batt=n     "
     exit 0
 else
     ACPI_OUT=$(acpi -b | head -n1)
@@ -9,15 +9,15 @@ else
     CAP_PAD=$(printf "%3d" "$CAP")
     if echo "$ACPI_OUT" | grep -F ", Charging," > /dev/null || echo "$ACPI_OUT" | grep -q "[: ]Charging,"; then
         if [ "$CAP" -ge 40 ]; then
-            echo "AC=y ; batt=y $CAP_PAD%"
+            echo "AC=y batt=y $CAP_PAD%"
         else
-            echo "AC=y ; batt=y $CAP_PAD%"
+            echo "AC=y batt=y $CAP_PAD%"
         fi
     else
         if [ "$CAP" -ge 40 ]; then
-            echo "AC=n ; batt=y $CAP_PAD%"
+            echo "AC=n batt=y $CAP_PAD%"
         else
-            echo "AC=n ; batt=y $CAP_PAD%"
+            echo "AC=n batt=y $CAP_PAD%"
         fi
     fi
 fi
